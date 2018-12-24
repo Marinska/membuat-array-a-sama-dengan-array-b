1f539ae501b6477de0b8266df66e4c40538ed8e5
Marinska <Marinska354@users.noreply.github.com>
2018-12-24 17:12:03 +0700

    content

--- array a dan b.cpp
+++ array a dan b.cpp
@@ -1,37 +1,27 @@
 #include <iostream> //Nama Umar Ibnu Zainal M
 using namespace std; //NIM 311810909
-
-void logika()  //dari soal array a = array b
-{ 				//akan dibuat array b = nilai genap dari nilai array a
+void logika()
+{
 	int a[5];
 	int b[5];
-	int x=1; //variable pengganti looping array a
-	for(int i = 1;i<=5;i++)
+	for(int i=1;i<=5;i++)
 	{
 		cout<<"Masukan nilai indeks ke-"<<i-1<<" : ";
 		cin>>a[i];
-		if(a[i]%2==0)
-		{
-			b[x]=a[i];
-			x=x+1;
-		}
+		b[i]=a[i];
 	}
-	cout<<"Array a[5] = {";
+	cout<<"Array A = {";
 	for(int j=1;j<=5;j++)
-	{
-		cout<<a[j]<<",";
-	}
-	cout<<"\b}\n";
-	
-	cout<<"Array b[5] = {";
+		{
+			cout<<a[j]<<",";
+		}
+			cout<<"\b}";
+	cout<<"\nArray B = {";
 	for(int k=1;k<=5;k++)
-	{
-		if(b[k]==0)
-		cout<<"_,";
-		else
-		cout<<b[k]<<",";
-	}
-	cout<<"\b}";
+		{
+			cout<<b[k]<<",";
+		}
+			cout<<"\b}";
 }
 
 int main()
